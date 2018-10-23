fdba092b95521c186946b610ad36d0e4811b30d4
Max Schwartz <schwam4@rpi.edu>
2018-10-23 19:53:52 -0400

    Add more basic class stuff.

---
+++ Board.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "board.h"
+
+Board::Board() {
+
+}
+
+void Board::print() const {
+
+}
--- Board.h
+++ Board.h
@@ -1,20 +1,27 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
 
 #include <string>
+#include <vector>
+#include <iostream>
+
 
 using namespace std;
 
 // For storing the nurikabe board and doing operations upon it.
 class Board {
 	public:
+		// Default Constructor
+		Board();
 
-
-		fancy_print() {
-			
-		}
+		// Prints out the board
+		void print() const;
 
 	private:
 		// Structure for holding the board
-		vector<vector<int> > m_board;
+		vector<vector<int> > board;
 		int height;
 		int width;
-}
+};
+
+#endif
