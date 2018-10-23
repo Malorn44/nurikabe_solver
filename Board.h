138b8f4639d099211ab68337b7b1bf8d7e81cbc1
Max Schwartz <schwam4@rpi.edu>
2018-10-23 19:35:10 -0400

    Added generic class structure

---
+++ Board.h
@@ -0,0 +1,20 @@
+
+#include <string>
+
+using namespace std;
+
+// For storing the nurikabe board and doing operations upon it.
+class Board {
+	public:
+
+
+		fancy_print() {
+			
+		}
+
+	private:
+		// Structure for holding the board
+		vector<vector<int> > m_board;
+		int height;
+		int width;
+}
